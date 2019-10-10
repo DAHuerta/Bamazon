daa81ad248877425171bc82a10b95c1b9425d9b6
Daniel <dhuerta1408@gmail.com>
2019-10-09 18:11:59 -0600

    finished inventory function

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,3 +20,23 @@ connection.connect(function(err) {
 
 });
 
+function inventory(){
+    
+connection.query(`SELECT * FROM products`, function(err, res){
+    if (err) throw err
+
+    var showInventory = new table ({
+        head: ["Item ID", "Product Name", "Department", "Price", "Stock"],
+        colWidths: [10,25,25,10,10],
+    });
+
+    for (var i = 0; i < res.length; i++) {
+        showInventory.push(
+            [res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+        );
+    };
+    console.log(showInventory.toString());
+    updates()
+});
+};
+
